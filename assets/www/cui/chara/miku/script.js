1da2b308d7c35ec3bce032e0a1b97fab537a7ae8
koduki <pascalm3@gmail.com>
2012-06-28 21:14:49 +0900

    add ajax rss support.

--- assets/www/cui/chara/miku/script.js
+++ assets/www/cui/chara/miku/script.js
@@ -58,7 +58,44 @@ $(function() {
     // main
     character.dlg.show("おはようございますー♪");
 
-    $('.flickSimple').hide()
+    var FlickWindow = function(height, width) {
+        var obj = $('<div id="firstitem" class="flickSimple landscape" style="overflow: hidden; "><ul /></div>');
+        $("body").append(obj);
+        $('.flickSimple').hide()
+
+        this.add = function(title, url, body) {
+            var article = $("<article><h1><a></a></h1><p></p>");
+            article.find("h1 a").text(title)
+            article.find("h1 a").attr("href", url)
+            article.find("p").text(body)
+            article.css("height", height + "px");
+
+            var item = $("<li>")
+            item.css("width", width + "px");
+            item.append(article)
+            obj.find("ul").append(item)
+          //  obj.find("ul").css("width", width * obj.find("li").size() + "px")
+
+            $(".flickSimple").css("width", width + "px")
+        }
+    }
+    var flickWindow = new FlickWindow(180, 300);
+    var url = 'https://twitter.com/statuses/user_timeline/82492709.rss'
+    $.get(url, function(res, code) {
+        var xml = $(res);
+        var items = xml.find("item").map(function(i, x) {
+            return {
+                "title" : $(x).find("title").text(),
+                "link" : $(x).find("link").text(),
+                "description" : $(x).find("description").text(),
+            }
+        })
+
+        items.each(function(i, item) {
+            flickWindow.add(item.title, item.link, item.description)
+        })
+    });
+
     window.onHome = function() {
         $('.flickSimple').fadeOut()
         character.dlg.show("おはようございますー♪");
