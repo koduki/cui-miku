44d6c0dfce0ebb17adb2ac843c8d36722fa80a36
koduki <pascalm3@gmail.com>
2012-07-01 11:50:09 +0900

    support recognizeSpeech.

--- src/cn/orz/pascal/cui/MainActivity.java
+++ src/cn/orz/pascal/cui/MainActivity.java
@@ -3,13 +3,12 @@ package cn.orz.pascal.cui;
 import org.apache.cordova.DroidGap;
 
 import android.os.Bundle;
- 
+
 public class MainActivity extends DroidGap {
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        super.loadUrl("file:///android_asset/www/index.html");
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
 
 }
