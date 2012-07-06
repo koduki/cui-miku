295c8503d938c720a2f3ade75626519eee270676
koduki <pascalm3@gmail.com>
2012-07-07 00:03:16 +0900

    add JS test.

--- test/MorphologicalAnalysiser.java
+++ test/MorphologicalAnalysiser.java
@@ -1,14 +1,22 @@
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.junit.Test;
 import net.reduls.sanmoku.Tagger;
 import net.reduls.sanmoku.Morpheme;
 import net.reduls.sanmoku.FeatureEx; // 追加
 
 public class MorphologicalAnalysiser {
-
+public static void main(String[] args) {
+	System.out.println("Hello World.");
+	new MorphologicalAnalysiser().test();
+}
 	@Test
 	public void test() {
+		System.out.println("Hello World.");
 		analyze("近くのラーメン屋を探して");
 		analyze("近くにあるラーメン屋を探して");
 		analyze("近くのラーメン屋を教えて");
@@ -25,9 +33,9 @@ public class MorphologicalAnalysiser {
 
 	@Test
 	public void test2() {
-		System.out.println(search("近くのラーメン屋を探して"));
-		String keyword = new Template().with("近く").and("探す")
-				.result(type("名詞").and("一般").and(not("接尾"))).apply();
+//		System.out.println(search("近くのラーメン屋を探して"));
+//		String keyword = new Template().with("近く").and("探す")
+//				.result(type("名詞").and("一般").and(not("接尾"))).apply();
 	}
 
 	public class Template {
@@ -64,25 +72,41 @@ public class MorphologicalAnalysiser {
 		boolean flag1 = false;
 		boolean flag2 = false;
 		String keyword = null;
+		Map<String, Boolean> baseforms = new HashMap<String, Boolean>(); 
+		baseforms.put("近く", false);
+		baseforms.put("探す", false);
+		
+		Map<String, Boolean> keywordPredicate = new HashMap<String, Boolean>();
+		keywordPredicate.put("名詞", true);
+		keywordPredicate.put("一般", true);
+		keywordPredicate.put("接尾", false);
+		
+		
 		for (Morpheme m : Tagger.parse(text)) { // 形態素解析
 			FeatureEx fe = new FeatureEx(m); // 解析結果の形態素インスタンスから、追加の素性データを取得
-
-			if (fe.baseform.equals("近く")) {
-				flag1 = true;
+			
+			if (baseforms.containsKey(fe.baseform)) {
+				baseforms.put(fe.baseform, true);
 			}
-			if (fe.baseform.equals("探す")) {
-				flag2 = true;
-			}
-			if (m.feature.indexOf("名詞") != -1 && m.feature.indexOf("一般") != -1
-					&& m.feature.indexOf("接尾") == -1) {
-				keyword = m.surface;
-			}
+			
+			keyword = applyKeyword(keywordPredicate, m);
 		}
+		
 		if (flag1 && flag2 && keyword != null) {
 			return keyword;
 		} else {
 			return null;
 		}
+	}
+
+	private String applyKeyword(Map<String, Boolean> keywordPredicate,
+			Morpheme m) {
+		for(Entry<String, Boolean> predicate:keywordPredicate.entrySet()){
+			if((m.feature.indexOf(predicate.getKey()) == -1) == predicate.getValue()){
+				return "";
+			}
+		}
+		return m.surface;
 	}
 
 	private void analyze(String text) {
