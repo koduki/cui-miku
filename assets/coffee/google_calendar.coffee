805e5bec924863cbee60223bfbcef87a5e8f3302
koduki <pascalm3@gmail.com>
2012-08-19 19:03:29 +0900

    support google calendar api.

--- assets/coffee/google_calendar.coffee
+++ assets/coffee/google_calendar.coffee
@@ -3,31 +3,25 @@ class window.GoogleCalendar
     @client_id = "150899810974-qv8fu9ck15n5cmojbrfd1r8o4l0leatk.apps.googleusercontent.com"
     @scope = "https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar"
     @client_secret = 'NuHzWDgOqRQ3HIbtrTaJtZjE'
+
+  authorize:(success) => 
+    url = "https://accounts.google.com/o/oauth2/auth?scope=#{@scope}&state=%2Fprofile&redirect_uri=https%3A%2F%2Flocalhost%2Foauth2callback&response_type=code&client_id=#{@client_id}&access_type=offline";
     window.plugins.childBrowser.onLocationChange = (loc) =>
       code_match = loc.match(/&code=(.+)&*/)
       if code_match
         code = code_match[1]
-        alert(code)
         console.log(code)
         window.plugins.childBrowser.close()
-        @getRefreshToken(code)
-        #@getCalendarList(access_token)
+        @getRefreshToken(code, success)
       else
         console.log("LocalChange unmatch:" + loc);
-
-  authorize:() => 
-    url = "https://accounts.google.com/o/oauth2/auth?scope=#{@scope}&state=%2Fprofile&redirect_uri=https%3A%2F%2Flocalhost%2Foauth2callback&response_type=code&client_id=#{@client_id}&access_type=offline";
+ 
     window.plugins.childBrowser.showWebPage url,{
       showLocationBar : false
     }
 
-  getCalendarList:(access_token) => 
-    url = 'https://www.googleapis.com/calendar/v3/users/me/calendarList'
-    @callAPI url, access_token, {}, (data) ->
-      alert(data.items[0].description)
-      onsole.log(data.items[0].description)
-
-  getRefreshToken:(code) =>
+  getRefreshToken:(code, success) =>
+    console.log("start:getRefreshToken")
     $.ajax
       type:'post'
       url : 'https://accounts.google.com/o/oauth2/token'
@@ -40,13 +34,32 @@ class window.GoogleCalendar
       dataType : 'json'
       success:(data) =>
         window.resultStatus = data
-        alert(data.refresh_token)
-        alert(data.access_token)
+        refresh_token = data.refresh_token
+        window.localStorage.setItem("googleapi.calendar.refresh_token", refresh_token)
+        success(refresh_token)
       error:(jqXHR, textStatus, errorThrown) =>
         window.errorStatus = jqXHR
-        console.log("failed:call google api:" + ", " + code + ","+ @client_id + ", " + @client_secret + ", " + errorThrown)
+        console.log("failed:getRefreshToken:" + ", " + code + ","+ @client_id + ", " + @client_secret + ", " + errorThrown)
 
-  callAPI:(url, access_token, data, success) => 
+  getAccessToken:(refresh_token, success) =>
+    console.log("start:getAccessToken")
+    $.ajax
+      type:'post'
+      url : 'https://accounts.google.com/o/oauth2/token'
+      data : 
+        'client_id':@client_id
+        'client_secret':@client_secret
+        'grant_type':'refresh_token'
+        'refresh_token':refresh_token
+      dataType : 'json'
+      success:(data) =>
+        window.resultStatus = data
+        success(data.access_token)
+      error:(jqXHR, textStatus, errorThrown) =>
+        window.errorStatus = jqXHR
+        console.log("failed:getAccessToken :" + ", " + refresh_token + ","+ @client_id + ", " + @client_secret + ", " + errorThrown)
+
+  callAPI:(url, access_token, data, success) =>
     $.ajax
       url : url
       data : data
@@ -54,4 +67,24 @@ class window.GoogleCalendar
       dataType : 'json',
       success : (data) => success(data)
       error:(jqXHR, textStatus, errorThrown) =>
+        window.errorStatus = jqXHR
         console.log("failed:call google api:" + url + ", " + access_token + ","+ jqXHR +", " + textStatus + ", " + errorThrown)
+
+  executeAPI:(url, data, success) =>
+    refresh_token = window.localStorage.getItem("googleapi.calendar.refresh_token")
+
+    if refresh_token == null
+      console.log("not found refresh token")
+      @authorize (refresh_token) => 
+        @getAccessToken refresh_token, (access_token) =>
+          @callAPI(url, access_token, data, success)
+    else
+      console.log("found refresh token")
+      @getAccessToken refresh_token, (access_token) =>
+        @callAPI(url, access_token, data, success)
+
+  getCalendarList:() => 
+    url = 'https://www.googleapis.com/calendar/v3/users/me/calendarList'
+    @executeAPI url, {}, (data) ->
+      alert(data.items[0].description)
+      console.log(data.items[0].description)
--- assets/www/js/google_calendar.js
+++ assets/www/js/google_calendar.js
@@ -4,52 +4,46 @@
   window.GoogleCalendar = (function() {
 
     function GoogleCalendar() {
+      this.getCalendarList = __bind(this.getCalendarList, this);
+
+      this.executeAPI = __bind(this.executeAPI, this);
+
       this.callAPI = __bind(this.callAPI, this);
+
+      this.getAccessToken = __bind(this.getAccessToken, this);
 
       this.getRefreshToken = __bind(this.getRefreshToken, this);
 
-      this.getCalendarList = __bind(this.getCalendarList, this);
-
       this.authorize = __bind(this.authorize, this);
-
-      var _this = this;
       this.client_id = "150899810974-qv8fu9ck15n5cmojbrfd1r8o4l0leatk.apps.googleusercontent.com";
       this.scope = "https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar";
       this.client_secret = 'NuHzWDgOqRQ3HIbtrTaJtZjE';
+    }
+
+    GoogleCalendar.prototype.authorize = function(success) {
+      var url,
+        _this = this;
+      url = "https://accounts.google.com/o/oauth2/auth?scope=" + this.scope + "&state=%2Fprofile&redirect_uri=https%3A%2F%2Flocalhost%2Foauth2callback&response_type=code&client_id=" + this.client_id + "&access_type=offline";
       window.plugins.childBrowser.onLocationChange = function(loc) {
         var code, code_match;
         code_match = loc.match(/&code=(.+)&*/);
         if (code_match) {
           code = code_match[1];
-          alert(code);
           console.log(code);
           window.plugins.childBrowser.close();
-          return _this.getRefreshToken(code);
+          return _this.getRefreshToken(code, success);
         } else {
           return console.log("LocalChange unmatch:" + loc);
         }
       };
-    }
-
-    GoogleCalendar.prototype.authorize = function() {
-      var url;
-      url = "https://accounts.google.com/o/oauth2/auth?scope=" + this.scope + "&state=%2Fprofile&redirect_uri=https%3A%2F%2Flocalhost%2Foauth2callback&response_type=code&client_id=" + this.client_id + "&access_type=offline";
       return window.plugins.childBrowser.showWebPage(url, {
         showLocationBar: false
       });
     };
 
-    GoogleCalendar.prototype.getCalendarList = function(access_token) {
-      var url;
-      url = 'https://www.googleapis.com/calendar/v3/users/me/calendarList';
-      return this.callAPI(url, access_token, {}, function(data) {
-        alert(data.items[0].description);
-        return onsole.log(data.items[0].description);
-      });
-    };
-
-    GoogleCalendar.prototype.getRefreshToken = function(code) {
+    GoogleCalendar.prototype.getRefreshToken = function(code, success) {
       var _this = this;
+      console.log("start:getRefreshToken");
       return $.ajax({
         type: 'post',
         url: 'https://accounts.google.com/o/oauth2/token',
@@ -62,13 +56,39 @@
         },
         dataType: 'json',
         success: function(data) {
+          var refresh_token;
           window.resultStatus = data;
-          alert(data.refresh_token);
-          return alert(data.access_token);
+          refresh_token = data.refresh_token;
+          window.localStorage.setItem("googleapi.calendar.refresh_token", refresh_token);
+          return success(refresh_token);
         },
         error: function(jqXHR, textStatus, errorThrown) {
           window.errorStatus = jqXHR;
-          return console.log("failed:call google api:" + ", " + code + "," + _this.client_id + ", " + _this.client_secret + ", " + errorThrown);
+          return console.log("failed:getRefreshToken:" + ", " + code + "," + _this.client_id + ", " + _this.client_secret + ", " + errorThrown);
+        }
+      });
+    };
+
+    GoogleCalendar.prototype.getAccessToken = function(refresh_token, success) {
+      var _this = this;
+      console.log("start:getAccessToken");
+      return $.ajax({
+        type: 'post',
+        url: 'https://accounts.google.com/o/oauth2/token',
+        data: {
+          'client_id': this.client_id,
+          'client_secret': this.client_secret,
+          'grant_type': 'refresh_token',
+          'refresh_token': refresh_token
+        },
+        dataType: 'json',
+        success: function(data) {
+          window.resultStatus = data;
+          return success(data.access_token);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+          window.errorStatus = jqXHR;
+          return console.log("failed:getAccessToken :" + ", " + refresh_token + "," + _this.client_id + ", " + _this.client_secret + ", " + errorThrown);
         }
       });
     };
@@ -86,8 +106,37 @@
           return success(data);
         },
         error: function(jqXHR, textStatus, errorThrown) {
+          window.errorStatus = jqXHR;
           return console.log("failed:call google api:" + url + ", " + access_token + "," + jqXHR(+", " + textStatus + ", " + errorThrown));
         }
+      });
+    };
+
+    GoogleCalendar.prototype.executeAPI = function(url, data, success) {
+      var refresh_token,
+        _this = this;
+      refresh_token = window.localStorage.getItem("googleapi.calendar.refresh_token");
+      if (refresh_token === null) {
+        console.log("not found refresh token");
+        return this.authorize(function(refresh_token) {
+          return _this.getAccessToken(refresh_token, function(access_token) {
+            return _this.callAPI(url, access_token, data, success);
+          });
+        });
+      } else {
+        console.log("found refresh token");
+        return this.getAccessToken(refresh_token, function(access_token) {
+          return _this.callAPI(url, access_token, data, success);
+        });
+      }
+    };
+
+    GoogleCalendar.prototype.getCalendarList = function() {
+      var url;
+      url = 'https://www.googleapis.com/calendar/v3/users/me/calendarList';
+      return this.executeAPI(url, {}, function(data) {
+        alert(data.items[0].description);
+        return console.log(data.items[0].description);
       });
     };
 
