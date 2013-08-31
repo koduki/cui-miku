72e189c09e3208d6cc8a2a0c5b77be3916fdeb8e
koduki <pascalm3@gmail.com>
2013-08-31 21:51:10 +0900

    modify run from background.

--- src/cn/orz/pascal/cui/MainActivity.java
+++ src/cn/orz/pascal/cui/MainActivity.java
@@ -8,10 +8,15 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends DroidGap {
+	private boolean isResumed;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+		Log.d("COLAS", "onCreate");
+
 		super.onCreate(savedInstanceState);
 
+		this.isResumed = false;
 		Intent intent = new Intent(MainActivity.this, EventService.class);
 		startService(intent);
 
@@ -20,6 +25,8 @@ public class MainActivity extends DroidGap {
 
 	@Override
 	protected void onNewIntent(Intent intent) {
+		Log.d("COLAS", "onNewIntent(" + this.isResumed + ")");
+
 		super.onNewIntent(intent);
 		setIntent(intent);
 		// now getIntent() should always return the last received intent
@@ -28,12 +35,13 @@ public class MainActivity extends DroidGap {
 	@Override
 	protected void onResume() {
 		super.onResume();
+
 		Intent intent = this.getIntent();
 		String result = intent.getStringExtra("event-result");
 		Log.d("COLAS", "onResume(" + result + ")");
-
-		if (result != null) {
+		if (result != null && !result.equals("")) {
 			super.loadUrl("javascript:alert('" + result + "')");
+			intent.putExtra("event-result", "");
 		}
 	}
 
